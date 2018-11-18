3a82c012cf4178e32f7408e7f7b73a7f9f575ece
morse <beats.by.morse@gmail.com>
2018-11-18 16:16:34 +0100

    initial

---
+++ tests/find-users-by-location.js
@@ -0,0 +1,34 @@
+// object i want
+
+
+
+{
+  posting: ["chiavari"]
+  users: [{
+    username: "",
+    id: 234234,
+    url: "",
+    followers: [{
+      username: "",
+      id: 2345,
+      url: ""
+    }, {
+      username: "",
+      id: 54345,
+      url: ""
+    }, {
+      username: "",
+      id: 2345,
+      url: ""
+    }, {
+      username: "",
+      id: 7990,
+      url: ""
+    }, {
+      username: "",
+      id: 2345,
+      url: ""
+    }],
+    following: []
+  }]
+}
