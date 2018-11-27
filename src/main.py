6440755c06fde3613125910f8f8f85922cfec0c4
remorses <beats.by.morse@gmail.com>
2018-11-27 20:22:50 +0100

    acc is right now

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ script = {}
 
 
 class Task():
-    def __init__(self, bot, id):
-        self._bot = bot
-        self.acc = bot.acc
+    def __init__(self, bot, id, acc=[]):
+        self.bot = bot
+        self.acc = acc
         self.id = id
 
 
@@ -30,26 +30,26 @@ if 'distributed' in script['mode']:
 
             for i, worker in workers:
                 worker.bot.start(options['nodes'], method,
-                                 worker.bot.acc, **options)
+                                 worker.acc, **options)
 
             for i, worker in workers.items():
-                worker.bot.acc.clear()
+                worker.acc.clear()
 
         elif 'from_nodes' in options:
 
             for i, node in enumerate(options['from_nodes']):
-                workers[i % len(workers)].bot.acc += [node]
+                workers[i % len(workers)].acc += [node]
 
             for i, worker in workers.items():
                 for edge in options['via_edges']:
                     threads.append(workers[i].bot.start(
-                        method, workers[i].bot.acc, **options))
+                        method, workers[i].acc, **options))
 
                 for t in threads:
                     t.join()
 
                 for i, worker in workers.items():
-                    worker.bot.acc.clear()
+                    worker.acc.clear()
 
 elif 'unison' in script['mode']:
     pass
