f31161f934f5e594bb468a14980ed143fa3cb984
remorses <beats.by.morse@gmail.com>
2018-11-29 18:10:54 +0100

    resolved merge

--- marionette/bot/bot.py
+++ marionette/bot/bot.py
@@ -29,10 +29,10 @@ class Bot:
         self._interactions = Interactions(self)
 
     def accumulate(self, x):
-            self.acc.append(x)
+        self.acc.append(x)
 
     def reset(self):
-            self.acc = []
+        self.acc = []
 
     def do(self, method, arg=[]):
         self.logger.info('doing {}'.format(method))
@@ -43,11 +43,7 @@ class Bot:
             t.start()
             return t
         elif method in methods(Interactions):
-<<<<<<< HEAD
             self.logger.info('doing an interaction {} with arg {} and acc {}'.format(
-=======
-            print('doing a {} interaction with arg {} and acc {}'.format(
->>>>>>> 9b00fb80d932c63bd6a181320040604f3f361857
                 method, arg, self.acc))
             t = Thread(target=self._interactions[method], args=(arg,))
             t.start()
