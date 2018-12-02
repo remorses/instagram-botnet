3f6c01cc0e66d211f2e917ff921bbb68c7d73377
Tommaso De Rossi <beats.by.morse@gmail.com>
2018-12-02 10:36:51 +0100

    added followers

--- marionette/reducer/methods.py
+++ marionette/reducer/methods.py
@@ -1,27 +1,46 @@
-methods = { 'author': author }
+methods = { 'authors': authors }
 
-def author(bot, nodes, amount, args):
+def authors(bot, nodes, amount, args) -> List[User]:
     
     authors = []
     
-    def author_from_id(id):
-            bot.api.media_info(id) 
-            user_id = bot.api.last_json["items"][0]["user"]["pk"] 
-            return User(id = user_id)
+    def _author(media):
+            bot.api.media_info(media.id) 
+            data = bot.api.last_json["items"][0]["user"]
+            id = data["pk"] 
+            return User(id=id, data=data)
             
-    def author_from_url(url): 
-            user_id = Media.id_from_url(url)
-            return author_from_id(user_id)
+    def _author_from_url(url): 
+            media = Media(url=url)
+            return author(media)
             
     if isinstance(node, Media):
-            authors += [author_from_id(node.id) for node in nodes]
+            authors += [_author(node) for node in nodes]
             
     else:
-            authors += [author_from_url(url) for url in nodes]
+            authors += [_author_from_url(url) for url in nodes]
             
     return authors
 
 
+def followers(bot, nodes, amount, args) -> List[User]:
+    
+    result = []
+    
+    def _followers(node) -> List[User]:
+            return [User(id=item['pk']) for item in self.api.get_total_followers(node.id, amount)]
+    
+    for node in nodes:
+    
+            if isinstance(node, User):    
+                        result += _followers(node)
+                        
+            else:
+                        user = User(name=node)
+                        result += _followers(user)
+            
+    return [user for user in result][::-1] if result else []
+    
 
 
 def make_methods(bot):
