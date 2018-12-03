eaa4de551faff331e8770bfe8aca29f1fbb20aa4
remorses <beats.by.morse@gmail.com>
2018-12-03 20:14:32 +0100

    fixed attribute error in dicts

--- marionette/reducer/state.py
+++ marionette/reducer/state.py
@@ -1,5 +1,18 @@
 class State(dict):
-    pass
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
 
 
 def make_state(task, bot):
--- marionette/task.py
+++ marionette/task.py
@@ -18,7 +18,21 @@ class Task(dict):
                     messages = []
                     stuff =    whokonws
     """
-    pass
+
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
 
 
 def make_task(data):
@@ -53,7 +67,8 @@ def partitionate(task: Task, bots):
 
     for (partition, bot) in enumerate(bots):
 
-        def _right_partition(i): return i % len(bots) == partition
+        def _right_partition(i):
+            return i % len(bots) == partition
         new_nodes = [node for (i, node) in enumerate(
             task.nodes) if _right_partition(i)]
         new_task = Task(nodes=new_nodes, amount=task.amount, args=task.args)
