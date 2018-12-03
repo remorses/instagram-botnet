a8567fc38cfadacc34df79575c2ff55b3f45e780
remorses <beats.by.morse@gmail.com>
2018-12-03 19:13:10 +0100

    made nodes a directory and changed to follow functional directives

---
+++ marionette/nodes/__init__.py
@@ -0,0 +1,20 @@
+
+from .node import Node
+from .user import User
+from .media import Media
+
+
+class Hashtag(Node):
+    pass
+
+
+class Geotag(Node):
+    pass
+
+
+class Usertag(Node):
+    pass
+
+
+class Comment(Node):
+    pass
