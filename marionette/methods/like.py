9ae31247b855fc51adf954f3d4ddb3f2daeb8283
remorses <beats.by.morse@gmail.com>
2018-12-03 21:05:33 +0100

    made like

--- marionette/methods/like.py
+++ marionette/methods/like.py
@@ -2,14 +2,13 @@ from typing import List
 from ..nodes import Node, User, Media
 
 
-def like(bot, nodes, amount, args) -> List[User]:
+def like(bot, nodes, amount, args):
 
     nodes = bot.filter(nodes)
 
     for node in nodes:
-
         if isinstance(node, Media):
-            pass
+            bot.api.like(node.id)
 
         else:
             raise Exception('{} isn\' a Media'.format(node))
