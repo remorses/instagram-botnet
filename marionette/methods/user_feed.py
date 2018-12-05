4a680976b2348607a1aa9c0c634f29eb2541bc30
remorses <beats.by.morse@gmail.com>
2018-12-05 21:13:19 +0100

    updated user_feed

--- marionette/methods/user_feed.py
+++ marionette/methods/user_feed.py
@@ -3,23 +3,16 @@ from typing import List
 from ..nodes import User, Media
 
 
-def user_feed(bot, nodes, amount, args) -> List[Media]:
+def userget_feed(bot, nodes, amount, args) -> List[Media]:
 
     result = []
 
     for node in nodes:
+        result += get_feed(bot, node, amount)
 
-        if isinstance(node, User):
-                    result += _feed(bot, node, amount)
-
-        elif isinstance(node, str):
-                    user = User(username=node)
-                    result += _feed(bot, user, amount)
-        else:
-            raise Exception
 
     return result
 
 
-def _feed(bot, node, amount) -> List[Media]:
+def get_feed(bot, node, amount) -> List[Media]:
     return [User(id=item['pk']) for item in bot.api.get_user_feed(node.id)][:amount]
