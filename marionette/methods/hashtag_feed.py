f43760115ab12ac8f8365c0dab50ad802d0173a0
remorses <beats.by.morse@gmail.com>
2018-12-12 18:44:25 +0100

    refactored haswhtag_feed

--- marionette/methods/hashtag_feed.py
+++ marionette/methods/hashtag_feed.py
@@ -1,39 +1,57 @@
 
 from typing import List
-from operator import concat
-from functools import reduce
-from ..nodes import User, Media, Hashtag
+from funcy import  rcompose, flatten, partial, print_calls
+from ..nodes import  Media, Hashtag
 from .common import accepts
-
 
 @accepts(Hashtag)
 def hashtag_feed(bot, nodes, amount, args) -> List[Media]:
 
-    result = []
 
-    result = [get_feed(bot, hashtag.name, amount) for hashtag in nodes]
+    get_items = rcompose(
+        lambda tag: tag.name,
+        lambda name: get_feed(name, bot=bot, amount=amount),
+    )
 
-    result = reduce(concat, result, [])
+    pack_media = rcompose(
+        lambda data: data['pk'],
+        lambda id: Media(id=id)
+    )
 
-    result = [Media(id=item['pk']) for item in result]
+    result = (pack_media(item) for user in nodes for item in get_items(user))
+
+    result = list(flatten(result))
 
     return result, bot.last
 
 
-def get_feed(bot, hashtag, amount) -> List[Media]:
 
-    hashtag_feed = []
+
+def get_feed(hashtag, bot , amount) -> List[Media]:
+
     next_max_id = ''
+    done = 0
 
     while True:
-        bot.api.get_hashtag_feed(hashtag, next_max_id)
-        if 'items' not in bot.last:
-            return hashtag_feed[:amount]
-        items = bot.last['items']
         try:
-            hashtag_feed += items
-            if not items or len(hashtag_feed) >= amount:
-                return hashtag_feed[:amount]
+            bot.api.get_hashtag_feed(hashtag, next_max_id)
+            items = bot.last["items"] if 'items' in bot.last else []
+
+            if len(items) <= amount:
+                yield from items
+                done += len(items)
+                return
+
+            if (done + len(items)) >= amount:
+                yield from items[:amount - done]
+                done += len(items)
+                return
+
+            else:
+                yield from items
+                done += len(items)
+
         except Exception:
-            return hashtag_feed[:amount]
+            return
+
         next_max_id = bot.last.get("next_max_id", "")
