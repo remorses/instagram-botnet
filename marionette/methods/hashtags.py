07a27dee1c5105110003707f9ac0fdaed690b7a7
remorses <beats.by.morse@gmail.com>
2019-01-02 23:48:19 +0100

    rmd

---
+++ marionette/methods/hashtags.py
@@ -0,0 +1,30 @@
+from funcy import rcompose
+from itertools import islice
+from ..nodes import Hashtag, Media
+from .common import accepts
+
+
+@accepts(Media)
+def authors(bot, nodes, amount, args):
+
+    _hashtags = rcompose(
+        lambda node: node.id,
+        lambda id: get_caption(bot, id),
+        lambda text: get_tags(text,)
+    )
+
+    pack_hashtag = lambda tag: Hashtag(name=tag)
+
+    result = (pack_hashtag(tag) for media in nodes for tag in _hashtags(media))
+    result = (tag for tag in result if bot.suitable(tag))
+    result = islice(result, amount)
+
+    return result, bot.last
+
+def get_tags(text):
+    yield from set(part[1:] for part in text.split() if part.startswith('#'))
+
+def get_caption(bot, id):
+    bot.api.media_info(id)
+    data = bot.last["items"][0]["caption"]['text']
+    return data
