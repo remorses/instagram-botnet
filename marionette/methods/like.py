2230a4afba0660bd6d7cb764bc2058d2e1a5d64a
remorses <beats.by.morse@gmail.com>
2019-01-03 16:32:57 +0100

    now interaction have an independent delay, (besides the one in the reducer)

--- marionette/methods/like.py
+++ marionette/methods/like.py
@@ -1,7 +1,7 @@
 from typing import List
 from .common import accepts, today
 from ..nodes import Node, User, Media
-
+from time import time
 @accepts(Media)
 def like(bot, nodes, amount, args):
 
@@ -9,9 +9,13 @@ def like(bot, nodes, amount, args):
         bot.api.like(media.id)
         if bot.last['status'] != 'ok':
             bot.logger.warn('request didn\'t return "ok" liking {}'.format(media.url))
+            time.sleep(bot.delay['error'])
         else:
             with bot.cache as cache:
                 cache['liked'].insert(dict(identifier=media.id, url=media.url, time=today(), type='media'))
+
+            bot.logger.debug('sleeping some time')
+            time.sleep(bot.delay['like'])
 
 
 
