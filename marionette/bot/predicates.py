6fc633930b8b3a6b7c794d3545e2cb7134e23277
remorses <beats.by.morse@gmail.com>
2019-01-03 15:16:21 +0100

    added geotag in predicates and added a type selector in db query

--- marionette/bot/predicates.py
+++ marionette/bot/predicates.py
@@ -1,35 +1,37 @@
 
-from ..nodes import Media, User, Hashtag, Geotag
+from ..nodes import Media, User, Hashtag, Geotag, Node
 
 def not_in_cache(bot, node):
 
     with bot.cache as cache:
         if isinstance(node, Media):
-            if cache['liked'].find_one(identifier=node.id):
+            if cache['liked'].find_one(identifier=node.id, type='media'):
                 return False
-            elif cache['commented'].find_one(identifier=node.id):
+            elif cache['commented'].find_one(identifier=node.id, type='media'):
                 return False
-            elif cache['reported'].find_one(identifier=node.id):
+            elif cache['reported'].find_one(identifier=node.id, type='media'):
                 return False
             else:
                 return True
 
 
         elif isinstance(node, User):
-            if cache['followed'].find_one(identifier=node.id):
+            if cache['followed'].find_one(identifier=node.id, type='user'):
                 return False
-            elif cache['blocked'].find_one(identifier=node.id):
+            elif cache['blocked'].find_one(identifier=node.id, type='user'):
                 return False
-            elif cache['messaged'].find_one(identifier=node.id):
+            elif cache['messaged'].find_one(identifier=node.id, type='user'):
                 return False
             else:
                 return True
-
 
         elif isinstance(node, Hashtag):
             return True
 
         elif isinstance(node, Geotag):
+            return True
+
+        elif isinstance(node, Node):
             return True
 
         else:
