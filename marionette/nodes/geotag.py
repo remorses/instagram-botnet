fabfb631d8158985f07ead446cc1a3c49adc4a47
remorses <beats.by.morse@gmail.com>
2019-01-03 15:09:28 +0100

    added node geotag

--- marionette/nodes/geotag.py
+++ marionette/nodes/geotag.py
@@ -1,50 +1,72 @@
 from .node import Node
 from .common import attributes
 
+"""
+	pk
+	name
+	address
+	city
+	short_name
+	lng
+	lat
+	external_source
+	facebook_places_id
+"""
 
-
-
+"""
+    "pk": 186886085560507,
+    "name": "Kartepe",
+    "address": "",
+    "city": "",
+    "short_name": "Kartepe",
+    "lng": 30.195368,
+    "lat": 40.675953,
+    "external_source": "facebook_places",
+    "facebook_places_id": 186886085560507
+"""
 
 class Geotag(Node):
 
-    def __init__(self, *, generic=None, name=None, id=None, data=None):
+    def __init__(self, *, generic=None, name=None, id=None, facebook_id=None, lng=None, lat=None, data=None):
 
         self._name = name
         self._id = id
+        self._facebook_id = facebook_id
+        self._lng = lng
+        self._lat = lat
+
         self._data = data
 
         if generic:
             self._name = generic
 
     def __repr__(self):
-        name, id, data = attributes(self)
+        name, id, *_ = attributes(self)
 
         if name:
-            return 'User(name=\'{}\')'.format(name)
+            return 'Geotag(name=\'{}\')'.format(name)
         elif id:
-            return 'User(id=\'{}\')'.format(id)
-        elif data:
-            return 'MediaUser(data=\'{...}\')'
+            return 'Geotag(id=\'{}\')'.format(id)
+        else:
+            return 'Geotag(...)'
 
     @property
     def name(self):
-        name, id, data = attributes(self)
+        name, id, data, *_ = attributes(self)
 
         if name:
             return name
-        elif id:
-            pass
         elif data:
-            return data['user']['name']
+            return data['name']
         else:
             return False
 
     @property
     def id(self):
-        name, id, data = attributes(self)
+        name, id, data, *_ = attributes(self)
         if id:
             return id
         elif data:
-            return data['user']['name']
+            return data['pk']
         else:
             return False
