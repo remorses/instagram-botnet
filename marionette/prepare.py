078212004af990a31f2fa7dd049555f7a99c5b92
remorses <beats.by.morse@gmail.com>
2019-01-04 14:34:06 +0100

    added docstr and refactoring

--- marionette/prepare.py
+++ marionette/prepare.py
@@ -9,13 +9,25 @@ def prepare(script):
 
     for bot in bots:
         if 'filter' in script:
-            # has_filter = bot.username in data['only'] or not data['only']
             bot.predicates += [make_predicate(script['filter'], bot)]
 
     return bots
 
 
 def make_bots(script):
+    """
+    max_per_day:
+        likes:          50
+        follow:         20
+        unfollow:       10
+        ...
+
+    delay:
+        likes:          10
+        usual:          2
+        ...
+
+    """
 
     bots = []
 
@@ -25,9 +37,9 @@ def make_bots(script):
     for bot in bots:
         if 'max_per_day' in script:
             bot.max_per_day = {key: value for key,
-                               value in script['max_per_day']}
+                               value in script['max_per_day'].items()}
         if 'delay' in script:
-            bot.delay = {key: value for key, value in script['delay']}
+            bot.delay = {key: value for key, value in script['delay'].items()}
 
     return bots
 
@@ -37,25 +49,25 @@ def make_predicate(script, bot):
     filter:
 
         user:
-                followers:      x > 50 and x < 1000
-                following:      x < 500
-                bio:            any(s not in ['porn', 'sex'] for s in x)
-                is_private:     x and False
-                is_business:    x and False
-                is_verified:    x and False
+            followers:      x > 50 and x < 1000
+            following:      x < 500
+            bio:            any(s not in ['porn', 'sex'] for s in x)
+            is_private:     x and False
+            is_business:    x and False
+            is_verified:    x and False
 
         media:
-                likes:          x < 200
-                comments:       x < 100
-                caption:        any(s not in x for s in ['porn', 'sex'])
+            likes:          x < 200
+            comments:       x < 100
+            caption:        any(s not in x for s in ['porn', 'sex'])
 
         hashtag:
-                name:           not in [porn, sex]
+            name:           not in [porn, sex]
 
         geotag:
-                name:           not in {{ cities }}
-                lat:            x < 1000 and x > 500
-                lng:            x < 1000 and x > 500
+            name:           not in {{ cities }}
+            lat:            x < 1000 and x > 500
+            lng:            x < 1000 and x > 500
 
 
     """
