1b41356732ba5d9944416ce59bd959f80739a435
remorses <beats.by.morse@gmail.com>
2019-01-04 12:26:55 +0100

    refactored

--- marionette/methods/user_feed.py
+++ marionette/methods/user_feed.py
@@ -3,21 +3,18 @@ from typing import List
 from funcy import  rcompose, flatten, partial, autocurry, fallback, cat, print_calls
 from itertools import islice
 from ..nodes import User, Media
-from .common import accepts, get_user_id
+from .common import accepts, get_cycled_api
 
 @accepts(User)
 def user_feed(bot, nodes, amount, args) -> List[Media]:
 
 
     get_items = rcompose(
-        lambda user: get_user_id(user, bot=bot) if not user.id else user.id,
+        lambda user: User.get_id(bot) if not user.id else user.id,
         lambda id: get_last_user_feed(id, bot=bot, amount=amount),
     )
 
-    pack_media = rcompose(
-        lambda data: data['pk'],
-        lambda id: Media(id=id)
-    )
+    pack_media = lambda data: Media(id=data['pk'], data=data)
 
     result = (pack_media(item) for user in nodes for item in get_items(user))
 
@@ -31,31 +28,4 @@ def user_feed(bot, nodes, amount, args) -> List[Media]:
 
 
 def get_last_user_feed(id, bot, amount, min_timestamp=None):
-
-    next_max_id = ''
-    done = 0
-
-    while True:
-
-        bot.api.get_user_feed(id, next_max_id, min_timestamp)
-        items = bot.last["items"] if 'items' in bot.last else []
-
-        if len(items) <= amount:
-            yield from items
-            done += len(items)
-            return
-
-        elif (done + len(items)) >= amount:
-            yield from items[:amount - done]
-            done += len(items)
-            return
-
-        elif not bot.last.get("more_available"):
-            yield from items
-            return
-
-        else:
-            yield from items
-            done += len(items)
-
-        next_max_id = bot.last.get("next_max_id", "")
+    return get_cycled_api(bot, bot.api.get_user_feed, id, 'items', amount)
