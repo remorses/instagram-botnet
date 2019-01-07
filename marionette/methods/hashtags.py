d67ef10e99cfeb09ae7cbad0ad6f14bcd8da5517
remorses <beats.by.morse@gmail.com>
2019-01-07 17:08:21 +0100

    rfctrd

--- marionette/methods/hashtags.py
+++ marionette/methods/hashtags.py
@@ -7,9 +7,7 @@ from .common import accepts
 @accepts(Media)
 def hashtags(bot, nodes,  args):
 
-    result = (tag for media in nodes for tag in media.get_hashtags(bot))
-    # result = (tag for tag in result if bot.suitable(tag))
-    result = (tag for tag in result if tag)
-    
+    process = lambda media: media.get_hashtags(bot)
+    result = map(process, nodes)
 
     return result, bot.last
