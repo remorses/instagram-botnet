16eda34294bb923f90bb309d4926cbfbd881e06a
remorses <beats.by.morse@gmail.com>
2019-01-07 17:04:54 +0100

    refactored geotag feed

--- marionette/methods/geotag_feed.py
+++ marionette/methods/geotag_feed.py
@@ -1,40 +1,24 @@
 
 from typing import List
-from funcy import  rcompose, flatten, partial, take
-from time import time
+from funcy import  rcompose, mapcat
 from ..nodes import  Media, Geotag
-from .common import accepts, cycled_api_call
+from .common import accepts, cycled_api_call, tap
 
 
-def tap(x, fun):
-    fun()
-    return x
 
 
 @accepts(Geotag)
 def geotag_feed(bot, nodes,  args) -> List[Media]:
 
+    pack_media = lambda data: Media(id=data['pk'], data=data)
 
-    _medias = rcompose(
+    process = rcompose(
         lambda tag: tag.id if tag.id else tag.get_id(bot),
         # lambda x: tap(x, lambda: print(bot.last)),
-        lambda id: get_feed(id, bot=bot),
+        lambda id: cycled_api_call(bot, bot.api.get_location_feed, id, 'items'),
+        lambda items: map(pack_media, items),
     )
 
-
-    pack_media = rcompose(
-        lambda data: Media(id=data['pk'], data=data)
-    )
-
-    result = (pack_media(item) for user in nodes for item in _medias(user))
-    # result = (node for node in result if bot.suitable(node))
-    result = (node for node in result if node)
-
+    result = mapcat(process, nodes)
 
     return result, bot.last
-
-
-
-
-def get_feed(geotag_id, bot ) -> List[Media]:
-    return cycled_api_call(bot, bot.api.get_location_feed, geotag_id, 'items')
