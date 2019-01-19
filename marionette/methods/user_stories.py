5e35b1de7dbf745e34ff51b8d6e3209eb7b4196f
remorses <beats.by.morse@gmail.com>
2019-01-19 21:22:28 +0100

    fixed taps

--- marionette/methods/user_stories.py
+++ marionette/methods/user_stories.py
@@ -11,14 +11,15 @@ def user_stories(bot, nodes,  args) -> List[Story]:
 
     pack_story = lambda data: Story()
     unmasked = lambda: unmask(json.dumps(bot.last, indent=4))
+    log_unmasked = lambda: bot.logger.warn(unmasked())
 
 
     process = rcompose(
         lambda user: user.id if user.id else user.get_id(bot),
-        lambda id: tap(id, bot.api.get_user_reel(id)),
-        lambda id: tap(id, bot.logger.warn(unmasked())),
-        lambda id: tap(id, bot.api.get_user_stories(id)),
-        lambda id: tap(id, bot.logger.warn(unmasked())),
+        lambda id: tap(id, lambda: bot.api.get_user_reel(id)),
+        lambda id: tap(id, log_unmasked),
+        lambda id: tap(id, lambda: bot.api.get_user_stories(id)),
+        lambda id: tap(id, log_unmasked),
         lambda items: map(pack_story, items),
     )
 
