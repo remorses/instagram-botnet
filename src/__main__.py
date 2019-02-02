ee52a0ab4b8ec0f684e0a2e3606351b5a8e9dbb5
remorses <beats.by.morse@gmail.com>
2019-02-02 20:09:47 +0100

    elaborated main

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,32 @@
 import sys
 import yaml
+import json
 from colorama import init, Fore
 from .execute import execute
 from .debug import unmask
 
 
+
+"""
+python -m src /etc/template.yml --data-file /etc/data.json
+
+"""
+import click
+# from instamob import execute
+
 if __name__ == '__main__':
-    file = open(sys.argv[-1], 'r')
-    script = yaml.load(file.read())
-    data = execute(script)
-    init()
-    print(Fore.YELLOW + unmask(data))
+    @click.command()
+    @click.argument('file', type=str)
+    @click.option('--data-file', help='data variables to add in the template file')
+    def main(ship, template_path, data_path):
+        template_file = open(template_path, 'r')
+        data_file = open(data_path, 'r')
+        template = yaml.load(template_file.read())
+        data = json.loads(data_file.read())
+        data_file.close()
+        template_file.close()
+
+        data = execute(template, data)
+        init()
+        print(Fore.YELLOW + unmask(data))
+        execute(template, data)
