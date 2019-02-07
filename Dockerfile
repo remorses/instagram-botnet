67f3dd025fccc117f57d68f4ab52c1302a50ebe1
remorses <beats.by.morse@gmail.com>
2019-02-07 14:30:12 +0100

    rename

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.6-alpine
 
 COPY requirements.txt /
 
-RUN pip install  --no-cache-dir -r /requirements.txt
+RUN pip install   instabotnet
 WORKDIR     /bot
 COPY        src /bot/src
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='instabotnet',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Instagram readable yaml rpc api for easy instagram scheduling and promotion',
     long_description=long_description,
