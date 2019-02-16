831c8922282f01868267fb569ca8cd22ba7354cc
remorses <beats.by.morse@gmail.com>
2019-02-16 13:55:38 +0100

    fix

--- instabotnet/methods/shuffle.py
+++ instabotnet/methods/shuffle.py
@@ -10,7 +10,7 @@ def shuffle(bot, nodes,  args) -> Node:
     max = args.get('max') # None works as inf
     batch = args.get('batch', max) # None works as inf
     nodes = take(batch, nodes)
-    if max <= len(nodes):
+    if max and max <= len(nodes):
         nodes = random.sample(nodes, k=max)
     else:
         nodes = random.sample(nodes, k=len(nodes))
