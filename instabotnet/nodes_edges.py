8ac3c5ce194bd60603b236af76a9866f62b61df0
remorses <beats.by.morse@gmail.com>
2019-02-16 13:25:20 +0100

    made calc_total_nodes

--- instabotnet/nodes_edges.py
+++ instabotnet/nodes_edges.py
@@ -27,7 +27,7 @@ def nodes_edges(body):
 
     info = dotdict(
         name=body['name'] if 'name' in body else 'not named',
-        total_nodes=reduce(lambda acc, e: e.args.get('amount', 1) * acc, edges, 1),
+        total_nodes=reduce(calc_total_nodes, edges, len(nodes)),
     )
 
     edges += [dotdict(type='evaluate', args=dict(info=info))]
@@ -38,7 +38,10 @@ def nodes_edges(body):
 
 
 
-
+def calc_total_nodes(acc, node):
+    amount  = node.args.get('amount', 1)
+    max = node.args.get('max', acc)
+    return amount * acc if acc < max else max
 
 
 def initialize_nodes(nodes, edges, data):
