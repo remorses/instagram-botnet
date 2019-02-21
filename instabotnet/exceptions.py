ac8193169ae0e2bb4ce6294b70bad4105a486b90
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-02-21 15:03:07 +0000

    dfdf wq::

--- instabotnet/exceptions.py
+++ instabotnet/exceptions.py
@@ -17,7 +17,7 @@ NetworkException
          *
          * Can be: "ok", "fail".
          */
-        'status'  => 'string',
+        'status'  : 'string',
         /*
          * Instagram's API failure error message(s).
          *
@@ -25,7 +25,7 @@ NetworkException
          * either a single string OR a data structure with multiple messages.
          * Our custom `getMessage()` will take care of parsing their value.
          */
-        'message' => 'string' | {'errors': []},
+        'message' : 'string' | {'errors': []},
         /*
          * This can exist in any Instagram API response, and carries special
          * status information.
@@ -33,7 +33,7 @@ NetworkException
          * Known messages: "fb_needs_reauth", "vkontakte_needs_reauth",
          * "twitter_needs_reauth", "ameba_needs_reauth", "update_push_token".
          */
-        '_messages' => 'Response\Model\_Message[]',
+        '_messages' : 'Response\Model\_Message[]',
 }
     
 
@@ -42,47 +42,47 @@ NetworkException
 # some endpoins like `login`, `users/check_username`, `users/check_email` returns an {error_type: ...} property,
 # others return {message: {errors: [...]}}
 # all direct send item istead returns {payload: {
-        'client_request_id' => 'string',
-        'client_context'    => 'string',
-        'message'           => 'string', # error message here
-        'item_id'           => 'string',
-        'timestamp'         => 'string',
-        'thread_id'         => 'string',
-        'canonical'         => 'bool',
-        'participant_ids'   => 'string[]'
+        'client_request_id' : 'string',
+        'client_context'    : 'string',
+        'message'           : 'string', # error message here
+        'item_id'           : 'string',
+        'timestamp'         : 'string',
+        'thread_id'         : 'string',
+        'canonical'         : 'bool',
+        'participant_ids'   : 'string[]'
 }
 
 
 EXCEPTION_MAP = {
-        'LoginRequiredException'       => ['login_required'],
-        'CheckpointRequiredException'  => [
+        'LoginRequiredException'       : ['login_required'],
+        'CheckpointRequiredException'  : [
             'checkpoint_required', // message
             'checkpoint_challenge_required', // error_type
         ],
-        'ChallengeRequiredException'   => ['challenge_required'],
-        'FeedbackRequiredException'    => ['feedback_required'],
-        'ConsentRequiredException'     => ['consent_required'],
-        'IncorrectPasswordException'   => [
+        'ChallengeRequiredException'   : ['challenge_required'],
+        'FeedbackRequiredException'    : ['feedback_required'],
+        'ConsentRequiredException'     : ['consent_required'],
+        'IncorrectPasswordException'   : [
             // "The password you entered is incorrect".
             '/password(.*?)incorrect/', // message
             'bad_password', // error_type
         ],
-        'InvalidSmsCodeException'      => [
+        'InvalidSmsCodeException'      : [
             // "Please check the security code we sent you and try again".
             '/check(.*?)security(.*?)code/', // message
             'sms_code_validation_code_invalid', // error_type
         ],
-        'AccountDisabledException'     => [
+        'AccountDisabledException'     : [
             // "Your account has been disabled for violating our terms".
             '/account(.*?)disabled(.*?)violating/',
         ],
-        'SentryBlockException'         => ['sentry_block'],
-        'InvalidUserException'         => [
+        'SentryBlockException'         : ['sentry_block'],
+        'InvalidUserException'         : [
             // "The username you entered doesn't appear to belong to an account"
             '/username(.*?)doesn\'t(.*?)belong/', // message
             'invalid_user', // error_type
         ],
-        'ForcedPasswordResetException' => ['/reset(.*?)password/'],
+        'ForcedPasswordResetException' : ['/reset(.*?)password/'],
 }
 
 
