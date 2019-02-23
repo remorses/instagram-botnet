a61b05415597057ce197b02e041e9085aff02bc7
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-02-23 17:55:36 +0100

    y

---
+++ benchmarks/memory.py
@@ -0,0 +1,22 @@
+from memory_profiler import profile 
+from .support import load_yaml
+import os
+
+USERNAME = os.environ.get('USERNAME')
+PASSWORD = os.environ.get('PASSWORD')
+
+
+
+@profile
+def main():
+
+    from instabotnet import execute
+    
+    execute(
+        load_yaml('test.yml'), 
+        dict(
+            username=USERNAME, 
+            password=PASSWORD
+        )
+    )
+    
