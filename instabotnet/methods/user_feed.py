a76c0a4bc8050f8f8fe330c7b13e63550791e413
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-02-23 17:02:01 +0100

    gh

--- instabotnet/methods/user_feed.py
+++ instabotnet/methods/user_feed.py
@@ -15,8 +15,8 @@ def user_feed(bot, nodes,  args) -> List[Media]:
     pack_media = lambda data: Media(id=data['pk'], data=data)
 
     process = rcompose(
-        lambda user: user.id if user.id else user.get_id(bot),
-        lambda id: cycled_api_call(amount, bot, bot.api.username_feed, id, 'items'),
+        lambda user: user.username,
+        lambda name: cycled_api_call(amount, bot, bot.api.username_feed, name, 'items'),
         lambda items: map(pack_media, items),
         # lambda x: tap(x, lambda: print(x)),
 
