5aaa4f1b4bd33cdb2d862809fdf539ce1b73df57
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-02-23 16:22:44 +0100

    erg

--- instabotnet/methods/user_feed.py
+++ instabotnet/methods/user_feed.py
@@ -9,18 +9,22 @@ from .common import cycled_api_call, decorate
 
 @decorate(accepts=User, returns=Media)
 def user_feed(bot, nodes,  args) -> List[Media]:
-    amount = args.get('amount')
+    try:
+        amount = args.get('amount')
 
-    pack_media = lambda data: Media(id=data['pk'], data=data)
+        pack_media = lambda data: Media(id=data['pk'], data=data)
 
-    process = rcompose(
-        lambda user: user.id if user.id else user.get_id(bot),
-        lambda id: cycled_api_call(amount, bot, bot.api.username_feed, id, 'items'),
-        lambda items: map(pack_media, items),
-        # lambda x: tap(x, lambda: print(x)),
+        process = rcompose(
+            lambda user: user.id if user.id else user.get_id(bot),
+            lambda id: cycled_api_call(amount, bot, bot.api.username_feed, id, 'items'),
+            lambda items: map(pack_media, items),
+            # lambda x: tap(x, lambda: print(x)),
 
-    )
+        )
 
-    result = mapcat(process, nodes)
+        result = mapcat(process, nodes)
 
-    return result, {}
+        return result, {}
+    except:
+        import traceback
+        traceback.print_exc()
