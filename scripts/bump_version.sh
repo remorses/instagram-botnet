c46af43097b9fdd1b5505a9f5420aac8fda95978
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-10 18:55:55 +0100

    kkk

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -8,3 +8,16 @@ docker create -v /app --name configs alpine:3.4 /bin/true
 docker cp . configs:/app
 
 docker run --rm  --volumes-from configs treeder/bump patch
+
+
+git config credential.helper 'cache --timeout=120'
+git config user.email $GITHUBEMAIL
+git config user.name $GITHUBPASSWORD
+
+git checkout master
+git status
+
+git add VERSION
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
