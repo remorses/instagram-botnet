7461856cc16f181a8ab7a01d39135db41dbbd304
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-10 19:00:46 +0100

    fixing

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,4 +1,4 @@
-set -ex
+
 cd `dirname $0`
 cd ../src
 
@@ -8,6 +8,7 @@ docker create -v /app --name configs alpine:3.4 /bin/true
 docker cp . configs:/app
 
 docker run --rm  --volumes-from configs treeder/bump patch
+docker copy treeder/bump:/app/VERSION VERSION
 
 
 git config credential.helper 'cache --timeout=120'
@@ -16,6 +17,8 @@ git config user.name $GITHUBPASSWORD
 
 git checkout master
 git status
+
+version=`cat VERSION`
 
 git add VERSION
 git commit -m "version $version"
