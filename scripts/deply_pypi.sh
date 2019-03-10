e9061a0405f92df7d4bb328f55610e4389adaa5e
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-03-10 17:00:27 +0100

    Create deply_pypi.sh

---
+++ scripts/deply_pypi.sh
@@ -0,0 +1,12 @@
+set -ex
+cd `dirname $0`
+cd ../src
+
+test -f ./VERSION || (echo "file VERSION containing current version is needed" && exit 1)
+
+python3 setup.py sdist bdist_wheel
+
+python3 -m twine upload  dist/*
+
+version=`cat VERSION`
+
