6744d72c96874b68deab1819bd1d80e0adafc495
remorses <beats.by.morse@gmail.com>
2019-03-10 21:53:39 +0100

    asdf

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -26,6 +26,6 @@ version=`cat VERSION`
 
 git add VERSION
 git commit -m "version $version"
-git tag  "version $version"
+git tag  "v$version"
 git tag  "[skip ci]"
-git push  https://${GITHUB_PERSONAL_TOKEN}@github.com/remorses/instagram-botnet.git "version $version"
+git push  https://${GITHUB_PERSONAL_TOKEN}@github.com/remorses/instagram-botnet.git --tags
