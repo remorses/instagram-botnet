6bb47a426461677725497d99e28d57042b418da3
remorses <beats.by.morse@gmail.com>
2019-03-10 17:53:15 +0100

    bo

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,4 +4,7 @@ cd ../src
 
 version=`cat VERSION`
 
-bumpversion --current-version $version minor ./VERSION
+docker create -v /app --name configs alpine:3.4 /bin/true
+docker cp . configs:/app
+
+docker run --rm  --volumes-from configs treeder/bump patch
