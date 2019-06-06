cf4f043055ae72987d8cee71c80c5357e8c2e23b
remorses <beats.by.morse@gmail.com>
2019-06-06 19:17:46 +0200

    nothing

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -24,3 +24,26 @@ def test_simple():
     data = dotdict()
     result = execute(template, data)
     print(json.dumps(result, indent=4))
+
+def test_follow():
+    template = """
+    bot:
+        username: {{ env.username }}
+        password: {{ env.password }}
+        settings_path: {{ env.username + '_settings.json' }}
+    actions:
+        -
+            name: 1
+            nodes: [instagram]
+            from: user
+            edges:
+                - followers:
+                    amount: 5
+                - scrape:
+                    model:
+                        username: x.username
+                    key: users
+    """
+    data = dotdict()
+    result = execute(template, data)
+    print(json.dumps(result, indent=4))
